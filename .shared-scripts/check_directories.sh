c3064fa4f98f13b57f75322de9b3bf56ebabe35b
Nikolay Martyanov <ohmspectator@gmail.com>
2023-10-02 13:53:36 +0200

    Handle Commits Individually in GitHub Actions.
    - Extracted the directory checking logic into a function called
      `check_directories` for better modularity.
    
    - Modified the "github-action" context to loop through each commit between
      `BASE_SHA` and `HEAD`, applying the directory check for each individual
      commit.
    
    The refactor allows the script to perform directory checks for each commit in a
    PR or push event when running as a GitHub Action. This ensures that even if the
    PR has multiple commits, each commit adheres to the directory structure
    guidelines.
    
    Signed-off-by: Nikolay Martyanov <ohmspectator@gmail.com>

--- .shared-scripts/check_directories.sh
+++ .shared-scripts/check_directories.sh
@@ -5,50 +5,53 @@ context=$1
 
 BASE_SHA=$2
 
-# Get the list of changed files based on the context
-if [[ $context == "pre-commit" ]]; then
-    # Get the list staged files
-    changed_files=$(git diff --cached --name-only)
-elif [[ $context == "github-action" ]]; then
-    # Check that the BASE_SHA is set
-    if [[ -z "${BASE_SHA}" ]]; then
-        echo "Error: BASE_SHA is not set"
-        exit 1
-    fi
-    # Always in detached HEAD state in GitHub Actions
-    current_commit=$(git rev-parse HEAD)
-    parent_commit=${BASE_SHA}
-    changed_files=$(git diff --name-only "${parent_commit}" "${current_commit}")
-else
-    echo "Error: Unknown context '$context'"
-    exit 1
-fi
+# Function to perform directory checks
+check_directories() {
+  local changed_files=$1
+  local changed_dirs=""
 
-# Initialize a string variable to keep track of changed directories
-changed_dirs=""
-
-# Loop through the list of changed files to check which directories are affected
-for file in $changed_files; do
+  for file in $changed_files; do
     echo "Checking file: $file"
     dir=$(dirname "$file")
     if [[ $dir == "." || ! $dir =~ ^(api|backend|frontend|deployment) ]]; then
-        dir="root"
+      dir="root"
     else
-        dir=$(echo "$dir" | cut -d'/' -f1)
+      dir=$(echo "$dir" | cut -d'/' -f1)
     fi
     if [[ ! $changed_dirs =~ (^|[[:space:]])$dir($|[[:space:]]) ]]; then
-        changed_dirs="$changed_dirs $dir"
+      changed_dirs="$changed_dirs $dir"
     fi
-done
+  done
 
-# Calculate the total number of directories affected
-total_changed=$(echo $changed_dirs | wc -w)
+  total_changed=$(echo $changed_dirs | wc -w)
 
-# If more than one directory is affected, prevent the commit
-if [ $total_changed -gt 1 ]; then
+  if [ $total_changed -gt 1 ]; then
     echo "Error: Commit affects files in multiple directories:$changed_dirs"
     echo "Please, commit files in one directory at a time. The repo most likely will be spit into multiple repos in the future."
     exit 1
+  fi
+}
+
+# Get the list of changed files based on the context
+if [[ $context == "pre-commit" ]]; then
+  changed_files=$(git diff --cached --name-only)
+  check_directories "$changed_files"
+elif [[ $context == "github-action" ]]; then
+  if [[ -z "${BASE_SHA}" ]]; then
+    echo "Error: BASE_SHA is not set"
+    exit 1
+  fi
+
+  commits=$(git rev-list --ancestry-path $BASE_SHA..HEAD)
+
+  for commit in $commits; do
+    echo "Checking commit: $commit"
+    changed_files=$(git diff-tree --no-commit-id --name-only -r $commit)
+    check_directories "$changed_files"
+  done
+else
+  echo "Error: Unknown context '$context'"
+  exit 1
 fi
 
 # Otherwise, allow the commit
