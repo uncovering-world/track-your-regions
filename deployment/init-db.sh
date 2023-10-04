4e965b2ccc485e354741ba2e1e8056712e5233b2
Nikolay Martyanov <ohmspectator@gmail.com>
2023-10-04 17:17:34 +0200

    deploy: Import the GADM db into a dedicated schema.
    Signed-off-by: Nikolay Martyanov <ohmspectator@gmail.com>

--- deployment/init-db.sh
+++ deployment/init-db.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 set -e
 
-# Import GPKG file into PostGIS
-ogr2ogr -f "PostgreSQL" PG:"dbname=$POSTGRES_DB user=$POSTGRES_USER password=$POSTGRES_PASSWORD" "/tmp/gadm.gpkg"
+# Connect to the PostgreSQL database and create the 'gadm' schema if it doesn't exist
+echo "Creating gadm schema..."
+psql -U "$POSTGRES_USER" -d "$POSTGRES_DB" -c "CREATE SCHEMA IF NOT EXISTS gadm;"
+
+# Import GPKG file into PostGIS, specify the schema as 'gadm'
+echo "Importing GPKG file into PostGIS..."
+ogr2ogr -f "PostgreSQL" PG:"dbname=$POSTGRES_DB user=$POSTGRES_USER password=$POSTGRES_PASSWORD" "/tmp/gadm.gpkg" -lco SCHEMA=gadm
+
+echo "Done."
+
