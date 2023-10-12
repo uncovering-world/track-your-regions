ea02333bc053590452b2276a114ba3ecc8f109de
Nikolay Martyanov <ohmspectator@gmail.com>
2023-10-12 12:43:12 +0200

    back: Refactor: Rename PORT to TEST_SERVER_PORT in .env.example and server.js.
    Signed-off-by: Nikolay Martyanov <ohmspectator@gmail.com>

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,7 @@
 const app = require('./app');
 
-const PORT = process.env.PORT || 3000;
+const TEST_SERVER_PORT = process.env.DB_PORT || 3000;
 
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+app.listen(TEST_SERVER_PORT, () => {
+    console.log(`Server running on port ${TEST_SERVER_PORT}`);
 });
