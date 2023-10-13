99ce886c3c8ead5c1fee70d9effefdde1ff3c70a
Nikolay Martyanov <ohmspectator@gmail.com>
2023-10-13 10:03:28 +0200

    back: Add express-validator for input validation in region routes.
    Added express-validator as a dependency in package.json.
    Integrated input validation in regionRoutes.js for 'regionId' parameter checks:
    - Routes affected: '/:regionId' and '/:regionId/subregions'.
    - Returns a 400 status code along with error messages for invalid input.
    
    Signed-off-by: Nikolay Martyanov <ohmspectator@gmail.com>

--- backend/src/routes/regionRoutes.js
+++ backend/src/routes/regionRoutes.js
@@ -1,10 +1,36 @@
 const express = require('express');
 const regionController = require('../controllers/regionController');
+const { check, validationResult } = require('express-validator');
 
 const router = express.Router();
 
 router.get('/root', regionController.getRootRegions);
-router.get('/:regionId', regionController.getRegionById);
-router.get('/:regionId/subregions', regionController.getSubregions);
+router.get('/:regionId',
+    ...[
+        check('regionId').isInt().withMessage('Region ID must be an integer')
+    ],
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            const errorMessages = errors.array().map(error => error.msg);
+            return res.status(400).json({errors: errorMessages});
+        }
+        await regionController.getRegionById(req, res);
+    }
+);
+router.get('/:regionId/subregions',
+    ...[
+        check('regionId').isInt().withMessage('Region ID must be an integer')
+    ],
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            const errorMessages = errors.array().map(error => error.msg);
+            return res.status(400).json({errors: errorMessages});
+        }
+        await regionController.getSubregions(req, res);
+    }
+);
+
 
 module.exports = router;
