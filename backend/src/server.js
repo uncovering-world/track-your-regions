2360f205bc65c24f8b647af0a136255fc6008dc7
Nikolay Martyanov <ohmspectator@gmail.com>
2023-10-16 13:42:34 +0200

    back: Fix the name of an env var parsed by the test server.
    Signed-off-by: Nikolay Martyanov <ohmspectator@gmail.com>

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,6 @@
 const app = require('./app');
 
-const TEST_SERVER_PORT = process.env.DB_PORT || 3000;
+const TEST_SERVER_PORT = process.env.TEST_SERVER_PORT || 3000;
 
 app.listen(TEST_SERVER_PORT, () => {
     console.log(`Server running on port ${TEST_SERVER_PORT}`);
