735bf0b91af6a2ec5c22aa27b8c52759553ab15c
Nikolay Martyanov <ohmspectator@gmail.com>
2023-10-25 18:26:08 +0200

    back: controllers: Implement `getGeometry` method for Region.
    Add a new controller method to retrieve and calculate the geometry of a region.
    Uses a recursive SQL query to combine the geometries of all sub-regions.
    
    Signed-off-by: Nikolay Martyanov <ohmspectator@gmail.com>

--- backend/src/controllers/regionController.js
+++ backend/src/controllers/regionController.js
@@ -1,4 +1,57 @@
 const { Region } = require('../models');
+const { Model, DataTypes, QueryTypes } = require('sequelize');
+const sequelize  = require("../config/db");
+
+
+exports.getGeometry = async (req, res) => {
+    const { regionId } = req.params;
+    const region = await Region.findOne({
+        where: { id: regionId }
+    });
+
+    if (!region) {
+        return res.status(404).json({ message: 'Region not found' });
+    }
+
+    let geometry = region.geom;
+
+    if (!geometry) {
+        const query = `
+            WITH RECURSIVE Subregions AS (
+                SELECT id, geom
+                FROM regions
+                WHERE id = :regionId
+                UNION ALL
+                SELECT r.id, r.geom
+                FROM regions r
+                INNER JOIN Subregions s ON r.parent_region_id = s.id
+                WHERE s.geom IS NULL
+            )
+            SELECT ST_AsGeoJSON(ST_Multi(ST_Union(geom))) as geometry
+            FROM Subregions
+            WHERE geom IS NOT NULL;
+        `;
+
+        const result = await sequelize.query(query, {
+            replacements: { regionId },
+            type: QueryTypes.SELECT
+        });
+
+        geometry = result.length > 0 ? result[0].geometry : null;
+
+        if (geometry) {
+            // Update the geometry in the database
+            await Region.update({ geom: geometry }, {
+                where: { id: regionId }
+            });
+        } else {
+            return res.status(404).json({ message: 'Geometry not found' });
+        }
+    }
+
+    return res.status(200).json({ geometry });
+};
+
 
 exports.getAncestors = async (req, res) => {
     const { regionId } = req.params;
