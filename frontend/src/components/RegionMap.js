6c359a3b18e6194ebff5607e383cec8846c47d72
Nikolay Martyanov <ohmspectator@gmail.com>
2023-11-02 16:46:00 +0100

    front: Add Map Display for Regions.
    - Integrate `maplibre-gl` into the project dependencies to enable map rendering
      capabilities.
    - Implement `RegionMap` component to display the geographic boundaries of
      selected regions.
    - Modify `MainDisplay` to include the `RegionMap` component, allowing users to
      visually explore selected regions.
    
    Issue: #34
    
    Signed-off-by: Nikolay Martyanov <ohmspectator@gmail.com>

---
+++ frontend/src/components/RegionMap.js
@@ -0,0 +1,80 @@
+import React, {useEffect, useRef} from 'react';
+import maplibregl from 'maplibre-gl';
+import {useRegion} from "./RegionContext";
+import {fetchRegionGeometry} from "../api";
+
+const MapComponent = () => {
+    const mapContainer = useRef(null);
+    const map = useRef(null);
+    const { selectedRegionId, selectedRegionName } = useRegion();
+
+    const fetchSelectedRegionGeometry = async () => {
+        if (selectedRegionId !== null && selectedRegionId !== 0) {
+            const response = await fetchRegionGeometry(selectedRegionId);
+            if (response) {
+                return response.geometry;
+            } else {
+                return null;
+            }
+        }
+    }
+
+    useEffect(() => {
+        if (map.current) return;
+
+        const initializeMap = async () => {
+            const polygonData = await fetchSelectedRegionGeometry();
+
+            if (!polygonData || !polygonData.coordinates) {
+                // Handle the case where there is no geometry data, perhaps set a default view?
+                console.log('No geometry data available for the selected region.');
+                return;
+            }
+
+            map.current = new maplibregl.Map({
+                container: mapContainer.current,
+                style: 'https://demotiles.maplibre.org/style.json', // specify the base map style
+                center: [
+                    polygonData.coordinates[0][0][0][0],
+                    polygonData.coordinates[0][0][0][1]
+                ], // center the map on the first coordinate of the polygon
+                zoom: 9
+            });
+
+            map.current.on('load', () => {
+                map.current.addSource('polygon', {
+                    type: 'geojson',
+                    data: {
+                        type: 'Feature',
+                        properties: {},
+                        geometry: polygonData // use the geometry from the API response
+                    }
+                });
+
+                map.current.addLayer({
+                    id: 'polygon',
+                    type: 'fill',
+                    source: 'polygon',
+                    layout: {},
+                    paint: {
+                        'fill-color': '#088', // fill color of the polygon
+                        'fill-opacity': 0.8
+                    }
+                });
+            });
+        };
+
+        initializeMap().then(r => console.log(r));
+
+        return () => {
+            if (map.current) {
+                map.current.remove();
+                map.current = null;
+            }
+        };
+    }, [selectedRegionName]);
+
+    return <div ref={mapContainer} style={{ width: '100%', height: '400px' }} />;
+};
+
+export default MapComponent;
