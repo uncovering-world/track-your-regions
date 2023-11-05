b7e8fcfdfa7908fb093701a3d07ac547cc715638
Nikolay Martyanov <ohmspectator@gmail.com>
2023-11-05 16:47:33 +0100

    deploy: Refactor Docker setup to allow optional init and external GADM file.
    - Removed the copying of GADM file into the Docker image in Dockerfile,
      instead, the file will be mounted from the host into the container using a
      volume in docker-compose.yml.
    - Introduced a new environment variable RUN_INIT to control the execution of
      the init-db script.
    - Updated Makefile to set RUN_INIT environment variable when starting the db
      service, and added a new target just-run-db for starting the db service without
      running the init script.
    - Updated init-db.sh to check the value of RUN_INIT before executing the
      script.
    
    This setup allows for more flexibility in managing the database initialization
    and GADM file, reducing the Docker image size and startup time.
    
    Signed-off-by: Nikolay Martyanov <ohmspectator@gmail.com>

--- deployment/init-db.sh
+++ deployment/init-db.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 set -e
 
+if [ "$RUN_INIT" != "true" ]; then
+    echo "Skipping init-db.sh"
+    exit 0
+fi
+
 echo "Creating database..."
 python3 -u /tmp/init-regions-table.py /data/gadm/gadm.gpkg
-
 echo "Done."
 
