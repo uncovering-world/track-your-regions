ad8b8a3816efaf5b86b858fa1ba8bd5e2f3ba5f0
Nikolay Martyanov <ohmspectator@gmail.com>
2023-11-05 13:11:57 +0100

    deploy: Optimize DB initialization with hierarchical processing and geometry data toggle.
    This commit restructures the database initialization process to address
    performance issues caused by the large GADM geo-dataset. By introducing a
    multi-step approach, we initially set up the core hierarchy without geometric
    data, resulting in faster initial processing. Post-hierarchy creation, we merge
    single-child nodes and adjust internal IDs for consistency. Furthermore, we
    introduce a toggle for deferring geometry data inclusion, facilitating a
    quicker setup for development and testing environments.
    
    Also: Add a new target 'db-shell' in Makefile for easier database access and
    modify 'init-db.sh' to streamline database creation logic.
    
    Issue: #58
    
    Signed-off-by: Nikolay Martyanov <ohmspectator@gmail.com>

--- deployment/init-db.sh
+++ deployment/init-db.sh
@@ -2,11 +2,10 @@
 set -e
 
 if [ "$RUN_INIT" != "true" ]; then
-    echo "Skipping init-db.sh"
-    exit 0
+  echo "Skipping init-db.sh"
+  exit 0
 fi
 
 echo "Creating database..."
-python3 -u /tmp/init-regions-table.py /data/gadm/gadm.gpkg
+python3 -u /tmp/init-regions-table.py /data/gadm/gadm.gpkg --geometry
 echo "Done."
-
