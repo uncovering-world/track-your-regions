3066f222defee7d12a7d6f8e5eef58eedcacb10f
Nikolay Martyanov <ohmspectator@gmail.com>
2023-11-23 19:40:14 +0100

    back: Implement Hierarchy and HierarchyNames Models.
    This commit introduces two new Sequelize models, `Hierarchy` and
    `HierarchyNames`, to the backend. These models are designed to handle the
    complex relationships and structures of regional data.
    
    - The `Hierarchy` model (Hierarchy.js) represents the hierarchical relationship
      of regions. It includes methods for transforming database entries to API
      format and for retrieving ancestor data of a given region. Fields include
      `regionId`, `parentId`, `hierarchyId`, `regionName`, and `hasSubregions`.
    
    - The `HierarchyNames` model (HierarchyNames.js) manages different types of
      hierarchies, such as administrative divisions. It also includes a method for
      API format transformation. This model primarily consists of `hierarchyId`,
      `hierarchyName`, and `isActive` fields.
    
    Additionally, the `index.js` file within the models directory has been updated
    to include these new models and establish their relationships with the existing
    `Region` model. This update introduces many-to-many and belongs-to
    associations.
    
    Signed-off-by: Nikolay Martyanov <ohmspectator@gmail.com>

---
+++ backend/src/models/Hierarchy.js
@@ -0,0 +1,77 @@
+const { Model, DataTypes, QueryTypes} = require('sequelize');
+const sequelize = require('../config/db');
+
+class Hierarchy extends Model {
+    // Transform the returned object to API format
+    toApiFormat() {
+        return {
+            id: this.regionId,
+            name: this.regionName,
+            hasSubregions: this.hasSubregions,
+        };
+    }
+    static async getAncestors(regionId, hierarchyId) {
+        const ancestors = await sequelize.query(`
+            WITH RECURSIVE Ancestors AS (
+                SELECT region_id, parent_id as parentRegionId, region_name
+                FROM hierarchy
+                WHERE region_id = :regionId AND hierarchy_id = :hierarchyId
+                UNION ALL
+                SELECT h.region_id, h.parent_id as parentRegionId, h.region_name
+                FROM hierarchy h
+                INNER JOIN Ancestors a ON h.region_id = a.parentRegionId
+            )
+            SELECT * FROM Ancestors;
+        `, {
+            replacements: { regionId, hierarchyId },
+            type: QueryTypes.SELECT
+        });
+        return ancestors.map(ancestor => {
+            return { id: ancestor.region_id, name: ancestor.region_name, parentRegionId: ancestor.parentRegionId}
+        });
+    }
+}
+
+Hierarchy.init({
+    regionId: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        field: 'region_id'
+    },
+    parentId: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+        references: {
+            model: 'hierarchy',
+            key: 'region_id'
+        },
+        field: 'parent_id'
+    },
+    hierarchyId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        field: 'hierarchy_id',
+        references: {
+            model: 'hierarchy_names',
+            key: 'hierarchy_id'
+        }
+    },
+    regionName: {
+        type: DataTypes.STRING(255),
+        allowNull: false,
+        field: 'region_name'
+    },
+    hasSubregions: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        field: 'has_subregions'
+    }
+}, {
+    sequelize,
+    modelName: 'Hierarchy',
+    tableName: 'hierarchy',
+    timestamps: false
+});
+
+module.exports = Hierarchy;
--- backend/src/models/index.js
+++ backend/src/models/index.js
@@ -1,7 +1,27 @@
 const Region = require('./Region');
+const Hierarchy = require('./Hierarchy');
+const HierarchyNames = require('./HierarchyNames');
+
+Hierarchy.belongsToMany(Region, {
+    through: 'hierarchy_region_mapping', // Name of the intermediate mapping table
+    foreignKey: 'alt_region_id', // Foreign key in the mapping table referring to AlternativeHierarchy
+    otherKey: 'region_id', // Foreign key in the mapping table referring to Region
+});
+
+Hierarchy.belongsTo(HierarchyNames, {
+    foreignKey: 'hierarchy_id',
+});
+
+Region.belongsToMany(Hierarchy, {
+    through: 'hierarchy_region_mapping', // Name of the intermediate mapping table
+    foreignKey: 'region_id', // Foreign key in the mapping table referring to Region
+    otherKey: 'alt_region_id', // Foreign key in the mapping table referring to Hierarchy
+});
 
 const models = {
     Region,
+    Hierarchy,
+    HierarchyNames,
 };
 
 module.exports = models;
