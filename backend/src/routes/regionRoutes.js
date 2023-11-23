f4e2b5deeb700cecd7a6518a3eb51df9d9f52e37
Nikolay Martyanov <ohmspectator@gmail.com>
2023-11-23 19:47:04 +0100

    back: Update Region Routes to Support Hierarchical Data and Validation.
    This commit updates the regionRoutes.js to enhance the routing logic with
    hierarchical data support and input validation. Key changes include:
    
    1. Introduction of a new route `/hierarchies` which utilizes the
       `getHierarchies` method from `regionController` to fetch and respond with
       hierarchy data.
    
    2. Modification of the `/root` route to include validation for an optional
       `hierarchyId` parameter, ensuring it must be an integer if provided. This route
       now uses the updated `getRootRegions` method from `regionController` that
       supports hierarchical data.
    
    3. Extension of the `/regions/:regionId/subregions` route to include an
       optional `hierarchyId` query parameter with similar validation as in the
       `/root` route. This change ensures that the request for subregions can be
       contextually filtered based on the hierarchy.
    
    4. The validation logic uses `express-validator` to check the correctness of
       input parameters, adding robustness to the API by preventing invalid requests.
    
    Signed-off-by: Nikolay Martyanov <ohmspectator@gmail.com>

--- backend/src/routes/regionRoutes.js
+++ backend/src/routes/regionRoutes.js
@@ -4,7 +4,23 @@ const { check, query, validationResult } = require('express-validator');
 
 const router = express.Router();
 
-router.get('/root', regionController.getRootRegions);
+router.get('/hierarchies',
+    async (req, res) => {
+        await regionController.getHierarchies(req, res);
+    }
+);
+router.get('/root',
+    ...[
+        check('hierarchyId').optional().isInt().withMessage('Hierarchy ID must be an integer'),
+    ], async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            const errorMessages = errors.array().map(error => error.msg);
+            return res.status(400).json({errors: errorMessages});
+        }
+        await regionController.getRootRegions(req, res);
+    }
+);
 router.get('/:regionId',
     ...[
         check('regionId').isInt().withMessage('Region ID must be an integer')
@@ -21,7 +37,8 @@ router.get('/:regionId',
 router.get('/:regionId/subregions',
     ...[
         check('regionId').isInt().withMessage('Region ID must be an integer'),
-        query('getAll').optional().isBoolean().withMessage('getAll must be a boolean')
+        query('getAll').optional().isBoolean().withMessage('getAll must be a boolean'),
+        query('hierarchyId').optional().isInt().withMessage('Hierarchy ID must be an integer'),
     ],
     async (req, res) => {
         const errors = validationResult(req);
@@ -29,7 +46,7 @@ router.get('/:regionId/subregions',
             const errorMessages = errors.array().map(error => error.msg);
             return res.status(400).json({errors: errorMessages});
         }
-        await regionController.getSubregions(req, res, req.query.getAll);
+        await regionController.getSubregions(req, res);
     }
 );
 
