e17d68daced14fbcb21e33cf2fe1205490edc2aa
Nikolay Martyanov <ohmspectator@gmail.com>
2023-11-30 13:12:43 +0100

    front: Remove Unused useState Import from NavigationPane.
    Updated 'NavigationPane.jsx' by removing the unused `useState` import from
    React.
    
    This change addresses the ESLint warning for unused imports, contributing to
    cleaner and more efficient code.
    
    Issue: #98
    
    Signed-off-by: Nikolay Martyanov <ohmspectator@gmail.com>

--- frontend/src/components/NavigationPane.jsx
+++ frontend/src/components/NavigationPane.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Box } from '@mui/material';
 import BreadcrumbNavigation from './BreadcrumbNavigation';
 import ListOfRegions from './ListOfRegions';
