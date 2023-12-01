4f7698a57bf43384d50a601108e6a7c4eee87d3a
Nikolay Martyanov <ohmspectator@gmail.com>
2023-12-01 22:27:57 +0100

    back: Apply ESLint Auto-Fixes for Airbnb Coding Standards.
    Adjusted various files across the backend, including `sequelizeConfig.js`,
    `app.js`, `db.js`, `regionController.js`, and others, to comply with Airbnb
    style guide enforced through ESLint.
    
    Changes include formatting adjustments, import reordering, and comment spacing
    for better readability and consistency.
    
    These modifications were auto-applied by ESLint to align our codebase with
    industry-standard coding practices.
    
    Issue: #98
    
    Signed-off-by: Nikolay Martyanov <ohmspectator@gmail.com>

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -2,29 +2,29 @@ const { Sequelize } = require('sequelize');
 
 // Get the db credentials from the .env file
 require('dotenv').config();
-//Load all present .env.* files
+// Load all present .env.* files
 for (const env of ['', '.development', '.production', '.local']) {
-    require('dotenv').config({ path: `.env${env}` });
+  require('dotenv').config({ path: `.env${env}` });
 }
 
 // Create a new Sequelize instance
 const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-        host:     process.env.DB_HOST || 'localhost',
-        dialect:  'postgres',
-    },
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST || 'localhost',
+    dialect: 'postgres',
+  },
 );
 
 // Test DB connection
 sequelize.authenticate()
-    .then(() => {
-        console.log('Database connection has been established successfully.');
-    })
-    .catch(err => {
-        console.error('Unable to connect to the database:', err);
-    });
+  .then(() => {
+    console.log('Database connection has been established successfully.');
+  })
+  .catch((err) => {
+    console.error('Unable to connect to the database:', err);
+  });
 
 module.exports = sequelize;
--- backend/src/server.js
+++ backend/src/server.js
@@ -3,5 +3,5 @@ const app = require('./app');
 const TEST_SERVER_PORT = process.env.TEST_SERVER_PORT || 3000;
 
 app.listen(TEST_SERVER_PORT, () => {
-    console.log(`Server running on port ${TEST_SERVER_PORT}`);
+  console.log(`Server running on port ${TEST_SERVER_PORT}`);
 });
