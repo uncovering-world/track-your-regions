ff5d71e0411d5151882957d9946d0bfc234cd2b6
Nikolay Martyanov <ohmspectator@gmail.com>
2023-12-03 14:03:17 +0100

    front: Refactor initializeMap function for enhanced readability and reusability.
    Moved `initializeMap` function outside of `useEffect` hook for improved
    readability and potential reuse.
    
    Signed-off-by: Nikolay Martyanov <ohmspectator@gmail.com>

--- frontend/src/components/RegionMap.jsx
+++ frontend/src/components/RegionMap.jsx
@@ -19,49 +19,49 @@ function MapComponent() {
     return null;
   };
 
+  const initializeMap = async () => {
+    const polygonData = await fetchSelectedRegionGeometry();
+
+    if (!polygonData || !polygonData.coordinates) {
+      // Handle the case where there is no geometry data, perhaps set a default view?
+      console.log('No geometry data available for the selected region.');
+      return;
+    }
+
+    map.current = new maplibregl.Map({
+      container: mapContainer.current,
+      style: 'https://demotiles.maplibre.org/style.json', // specify the base map style
+      center: [
+        polygonData.coordinates[0][0][0][0],
+        polygonData.coordinates[0][0][0][1],
+      ], // center the map on the first coordinate of the polygon
+      zoom: 9,
+    });
+
+    map.current.on('load', () => {
+      map.current.addSource('polygon', {
+        type: 'geojson',
+        data: {
+          type: 'Feature',
+          properties: {},
+          geometry: polygonData, // use the geometry from the API response
+        },
+      });
+
+      map.current.addLayer({
+        id: 'polygon',
+        type: 'fill',
+        source: 'polygon',
+        layout: {},
+        paint: {
+          'fill-color': '#088', // fill color of the polygon
+          'fill-opacity': 0.8,
+        },
+      });
+    });
+  };
+
   useEffect(() => {
-    const initializeMap = async () => {
-      const polygonData = await fetchSelectedRegionGeometry();
-
-      if (!polygonData || !polygonData.coordinates) {
-        // Handle the case where there is no geometry data, perhaps set a default view?
-        console.log('No geometry data available for the selected region.');
-        return;
-      }
-
-      map.current = new maplibregl.Map({
-        container: mapContainer.current,
-        style: 'https://demotiles.maplibre.org/style.json', // specify the base map style
-        center: [
-          polygonData.coordinates[0][0][0][0],
-          polygonData.coordinates[0][0][0][1],
-        ], // center the map on the first coordinate of the polygon
-        zoom: 9,
-      });
-
-      map.current.on('load', () => {
-        map.current.addSource('polygon', {
-          type: 'geojson',
-          data: {
-            type: 'Feature',
-            properties: {},
-            geometry: polygonData, // use the geometry from the API response
-          },
-        });
-
-        map.current.addLayer({
-          id: 'polygon',
-          type: 'fill',
-          source: 'polygon',
-          layout: {},
-          paint: {
-            'fill-color': '#088', // fill color of the polygon
-            'fill-opacity': 0.8,
-          },
-        });
-      });
-    };
-
     if (!map.current) {
       initializeMap().then((r) => console.log(r));
     }
