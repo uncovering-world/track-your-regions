46e1c32a1b2379f44fa9a26dbbf94e90e63f5193
Nikolay Martyanov <ohmspectator@gmail.com>
2023-12-08 06:25:40 +0100

    deploy: Refactor database dump and restoration process.
    This commit introduces several enhancements to the database management workflow
    in the Makefile, Dockerfile, and restore-db.sh script. The key changes include:
    
    1. In the Makefile:
       - Added 'export' at the beginning to ensure environment variables from .env
         files are available to the Makefile.
       - Updated the 'dump-db' target to specifically dump the 'public' schema, and
         added verbose output and flags to exclude owner and ACL for portability.
    
    2. In the Dockerfile:
       - Removed hardcoded environment variables for PostgreSQL credentials,
         ensuring they are dynamically passed at runtime.
       - Added ARG DB_DUMP_FILE at the beginning for clarity and consistency.
    
    3. In the restore-db.sh script:
       - Simplified the restoration process by removing the initial wait loop for
         PostgreSQL server readiness.
       - Utilized 'pg_restore' with verbose output for better visibility during
         restoration.
       - Removed the fallback to 'psql' for restoration, relying solely on
         'pg_restore'.
    
    These changes aim to enhance the flexibility and clarity of the database dump
    and restore processes, alongside improvements in handling environment
    variables.
    
    Issue: #101
    
    Signed-off-by: Nikolay Martyanov <ohmspectator@gmail.com>

--- deployment/db-image/restore-db.sh
+++ deployment/db-image/restore-db.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 set -e
 
-# Wait for the PostgreSQL server to start
-until pg_isready -h localhost -U $POSTGRES_USER -d $POSTGRES_DB; do
-  sleep 1
-done
-
 # Restore the database from the dump
-pg_restore -U $POSTGRES_USER -d $POSTGRES_DB /tmp/db.dump || \
-psql -U $POSTGRES_USER -d $POSTGRES_DB -f /tmp/db.dump
+pg_restore -v -U $POSTGRES_USER -d $POSTGRES_DB /tmp/db.dump
 
 # Remove the dump file
 rm /tmp/db.dump
