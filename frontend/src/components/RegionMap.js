2161b4093e40660dc41378eb918c0b47ef062c35
Pavel Belokon <pbelokon@myseneca.ca>
2023-12-12 18:21:10 -0500

    added a conditional dendering int he regionMap

---
+++ frontend/src/components/RegionMap.js
@@ -0,0 +1,95 @@
+import React, {useEffect, useRef, useState} from 'react';
+import maplibregl from 'maplibre-gl';
+import {useNavigation} from "./NavigationContext";
+import {fetchRegionGeometry} from "../api";
+import {Button} from '@mui/material';
+
+const MapComponent = () => {
+    const mapContainer = useRef(null);
+    const map = useRef(null);
+    const { selectedRegion, selectedHierarchy } = useNavigation();
+    const [hasGeometryData, setHasGeometryData] = useState(true);
+
+    const fetchSelectedRegionGeometry = async (force = false) => {
+        if (selectedRegion.id !== null && selectedRegion.id !== 0) {
+            const response = await fetchRegionGeometry(selectedRegion.id, selectedHierarchy.hierarchyId, force);
+            if (response) {
+                return response.geometry;
+            } else {
+                return null;
+            }
+        }
+    }
+
+    useEffect(() => {
+        if (map.current) return;
+
+        const initializeMap = async () => {
+            const polygonData = await fetchSelectedRegionGeometry();
+
+            if (!polygonData || !polygonData.coordinates) {
+                // Handle the case where there is no geometry data, perhaps set a default view?
+                console.log('No geometry data available for the selected region.');
+                setHasGeometryData(false);
+                return;
+            }
+
+            map.current = new maplibregl.Map({
+                container: mapContainer.current,
+                style: 'https://demotiles.maplibre.org/style.json', // specify the base map style
+                center: [
+                    polygonData.coordinates[0][0][0][0],
+                    polygonData.coordinates[0][0][0][1]
+                ], // center the map on the first coordinate of the polygon
+                zoom: 9
+            });
+
+            map.current.on('load', () => {
+                map.current.addSource('polygon', {
+                    type: 'geojson',
+                    data: {
+                        type: 'Feature',
+                        properties: {},
+                        geometry: polygonData // use the geometry from the API response
+                    }
+                });
+
+                map.current.addLayer({
+                    id: 'polygon',
+                    type: 'fill',
+                    source: 'polygon',
+                    layout: {},
+                    paint: {
+                        'fill-color': '#088', // fill color of the polygon
+                        'fill-opacity': 0.8
+                    }
+                });
+            });
+        };
+
+        initializeMap().then(r => console.log(r));
+
+        return () => {
+            if (map.current) {
+                map.current.remove();
+                map.current = null;
+            }
+        };
+    }, [selectedRegion, selectedHierarchy]);
+
+    const handleGetGeometry = () => {
+        fetchSelectedRegionGeometry(true);
+    }
+
+    return (
+        <div>
+            {hasGeometryData ? (
+                <div ref={mapContainer} style={{ width: '100%', height: '400px' }} />
+            ) : (
+                <Button onClick={handleGetGeometry}>Get Geometry</Button>
+            )}
+        </div>
+    );
+};
+
+export default MapComponent;
