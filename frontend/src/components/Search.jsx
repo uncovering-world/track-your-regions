4fbb8a010891ba5eecff3408e71a00076f0c542d
Nikolay Martyanov <ohmspectator@gmail.com>
2023-12-20 22:11:21 +0100

    front: Search Component Enhancement.
    The search component functionality was improved by making it more
    interactive and user-friendly. The search results now return the
    smallest unique path segment in case of duplicate names, enhancing user
    experience. The dropdown visibility is also managed based on the search
    results. This ensures that the dropdown only appears when there are
    actual search results. Additionally, the 'selectedRegion' state is now
    updated with detailed region information obtained from the API,
    providing more context to the user.
    
    Issue: #151
    
    Signed-off-by: Nikolay Martyanov <ohmspectator@gmail.com>

--- frontend/src/components/Search.jsx
+++ frontend/src/components/Search.jsx
@@ -1,13 +1,49 @@
 import React, { useState, useEffect } from 'react';
 import { Autocomplete, TextField } from '@mui/material';
-import { fetchSearchResults } from '../api';
+import { fetchSearchResults, fetchRegion } from '../api';
 import { useNavigation } from './NavigationContext';
 
 function Search() {
   const [searchTerm, setSearchTerm] = useState('');
   const [searchResults, setSearchResults] = useState([]);
-  const [inputValue, setInputValue] = useState('');
-  const { setSelectedRegion } = useNavigation();
+  const [inputValue, setInputValue] = useState({});
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const { selectedRegion, setSelectedRegion, selectedHierarchyId } = useNavigation();
+
+  function formatNames(foundResults) {
+    const nameCount = new Map();
+    foundResults.forEach((item) => {
+      nameCount.set(item.name, (nameCount.get(item.name) || 0) + 1);
+    });
+
+    return foundResults.map((item) => {
+      if (nameCount.get(item.name) === 1) {
+        return ({
+          name: item.name,
+          segment: null,
+          id: item.id,
+        }); // Unique name, return as is
+      }
+      // Find the smallest unique path segment
+      const pathSegments = item.path.split(' > ');
+      let uniqueSegment = pathSegments[pathSegments.length - 1];
+
+      for (let i = pathSegments.length - 2; i >= 0; i -= 1) {
+        const testPath = pathSegments.slice(i).join(' > ');
+        const isUnique = foundResults.filter((r) => r.path.includes(testPath)).length === 1;
+        if (isUnique) {
+          uniqueSegment = pathSegments.slice(i).join(' > ');
+          break;
+        }
+      }
+
+      return ({
+        name: item.name,
+        segment: uniqueSegment,
+        id: item.id,
+      });
+    });
+  }
 
   useEffect(() => {
     let active = true;
@@ -18,20 +54,27 @@ function Search() {
           const results = await fetchSearchResults(searchTerm);
           if (active) {
             setSearchResults(results);
+            if (results.length > 0) {
+              setIsDropdownOpen(true);
+            }
           }
         } catch (error) {
           console.error('Error fetching search results:', error);
         }
       } else {
         setSearchResults([]);
+        setIsDropdownOpen(false);
       }
     };
 
-    if (!searchTerm || searchTerm.length < 3) {
-      setSearchResults([]);
-      return () => {
-        active = false;
-      };
+    if (selectedRegion) {
+      if (!searchTerm || searchTerm.length < 3) {
+        setSearchResults([]);
+        setIsDropdownOpen(false);
+        return () => {
+          active = false;
+        };
+      }
     }
     const timerId = setTimeout(fetchResults, 500);
 
@@ -39,21 +82,56 @@ function Search() {
       active = false;
       clearTimeout(timerId);
     };
-  }, [searchTerm]);
+  }, [searchTerm, selectedRegion]);
 
   return (
     <Autocomplete
       id="search-autocomplete"
-      options={searchResults.map((option) => option.name)}
-      value={searchTerm}
-      onChange={(event, newValue) => {
-        const selectedRegion = searchResults.find((region) => region.name === newValue);
-        setSelectedRegion(selectedRegion);
+      options={searchResults.length > 0 ? formatNames(searchResults) : [inputValue]}
+      getOptionLabel={(option) => {
+        if (option && typeof option === 'object' && option.name) {
+          return option.segment ? `${option.name} (${option.segment})` : option.name;
+        }
+        return option;
       }}
-      inputValue={inputValue}
+      freeSolo
+      open={isDropdownOpen}
+      onOpen={() => {
+        if (searchResults.length > 0) {
+          setIsDropdownOpen(true);
+        }
+      }}
+      onClose={() => {
+        setIsDropdownOpen(false);
+      }}
+      value={searchTerm}
+      onChange={async (event, newValue) => {
+        if (!newValue) {
+          setSearchTerm('');
+          return;
+        }
+        const selectedItem = searchResults.find((region) => region.id === newValue.id);
+        const region = await fetchRegion(selectedItem.id, selectedHierarchyId);
+        const newRegion = {
+          id: region.id,
+          name: region.name,
+          info: region.info,
+          hasSubregions: region.hasSubregions,
+        };
+        setSelectedRegion(newRegion);
+        setIsDropdownOpen(false);
+      }}
+      inputValue={searchTerm}
       onInputChange={(event, newInputValue) => {
-        setInputValue(newInputValue);
-        setSearchTerm(newInputValue);
+        if (newInputValue.length === 0) {
+          console.log('newInputValue is empty');
+          setIsDropdownOpen(false);
+          return;
+        }
+        // find the region with the matching name
+        const matchingRegion = searchResults.find((region) => region.name === newInputValue);
+        setInputValue(matchingRegion);
+        setSearchTerm(matchingRegion ? matchingRegion.name : newInputValue);
       }}
       renderInput={(params) => (
         <TextField
