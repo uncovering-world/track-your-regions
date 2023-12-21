041257319b012616779cdee711b02de80b01f341
Nikolay Martyanov <ohmspectator@gmail.com>
2023-12-21 18:14:13 +0100

    back: Temporarily Disable CSRF Check in app.js.
    Disabled CSRF protection in backend/src/app.js with a TODO comment and a
    nosemgrep directive to suppress linter warnings. This is a temporary measure
    until CSRF protection can be properly implemented.
    
    Issue: #170
    
    Signed-off-by: Nikolay Martyanov <ohmspectator@gmail.com>

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,6 +3,9 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const routes = require('./routes');
 
+// TODO: Add CSRF protection, see GH Issue #170. Ignore for now.
+// eslint-disable-next-line max-len
+// nosemgrep: javascript.express.security.audit.express-check-csurf-middleware-usage.express-check-csurf-middleware-usage
 const app = express();
 
 app.use(cors());
